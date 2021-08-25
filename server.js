df7a464075e5ea892d055f121fc53efea8190691
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-08-25 22:17:50 +0000

    added the the accesstoken in server

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ const posts=[
 }
 ]
 
-app.get('/posts',(req,res)=>{
-res.json(post)
+app.get('/posts',authenticationToken(req,res)=>{
+
+res.json(posts.filter(post => post.username == req.user.name))
 })
 
 app.post('/login', (req,res)=>{
@@ -21,7 +22,18 @@ app.post('/login', (req,res)=>{
     const username  = req.body.username
     const user = { name:username}
 
-    jwt.sign(user ,process.env.ACCESS_TOKEN_SECRET)
+    const accessToken = jwt.sign(user ,process.env.ACCESS_TOKEN_SECRET)
     res.json({ accessTooken: accessToken})
 
-app.listen(3000);
+ function authenticationToken(req, res,next){
+     const authHeader = req.headers['authourization']
+     const token = authHeader && authHeader.split('')[1]
+     if(token == null) return  res.sendStatus(401)
+
+     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err ,user)=>{
+         if(err) return res.sendStatus(403)
+         req.user = user
+         next()
+     })
+ }
+app.listen(3000)
